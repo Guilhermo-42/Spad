054f6ffb9278acb6afce589ec62ad933ce294747
Guilherme <guilhermeasrgl9@gmail.com>
2018-09-02 11:06:50 -0300

    Updating versionCode to 0.1.1

--- app/src/main/java/duo/com/spad/flow/login/LoginPresenterImpl.kt
+++ app/src/main/java/duo/com/spad/flow/login/LoginPresenterImpl.kt
@@ -2,7 +2,6 @@ package duo.com.spad.flow.login
 
 import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
