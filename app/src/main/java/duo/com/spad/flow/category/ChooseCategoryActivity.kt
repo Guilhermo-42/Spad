7c89163a4072219e42dbc4b1efd8c19508db9da0
Guilherme <guilhermeasrgl9@gmail.com>
2018-09-22 13:39:22 -0300

    Adjustments on toolbar

--- app/src/main/java/duo/com/spad/flow/category/ChooseCategoryActivity.kt
+++ app/src/main/java/duo/com/spad/flow/category/ChooseCategoryActivity.kt
@@ -2,7 +2,9 @@ package duo.com.spad.flow.category
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import duo.com.spad.R
+import kotlinx.android.synthetic.main.activity_choose_category.*
 
 class ChooseCategoryActivity : AppCompatActivity() {
 
@@ -13,6 +15,23 @@ class ChooseCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_category)
+
+        setSupportActionBar(chooseCategoryToolbar)
+        supportActionBar?.let {
+            it.setHomeAsUpIndicator(R.drawable.ic_arrow_return)
+            it.setHomeButtonEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.title = getString(R.string.category)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                super.onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
